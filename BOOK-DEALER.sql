7c183c30b620cb14be216e2028540f135a2e5c25
Karthik-SK12345 <71306768+Karthik-SK12345@users.noreply.github.com>
2021-04-29 09:51:56 +0530

    Update BOOK-DEALER.sql

--- BOOK-DEALER.sql
+++ BOOK-DEALER.sql
@@ -1,103 +1,55 @@
-CREATE DATABASE Book_Dealer_Database;
-USE Book_Dealer_Database;
-CREATE TABLE Author(
-	author_id int,
-    A_name varchar(30),
-    city varchar(30),
-    country varchar(30),
-    PRIMARY KEY (author_id)
-);
-DESC Author;
+CREATE DATABASE BOOKDEALER;
+USE BOOKDEALER;
+CREATE TABLE AUTHOR(AUTHOR_ID INT,NAME VARCHAR(30),CITY VARCHAR(30),COUNTRY VARCHAR(30));
+CREATE TABLE PUBLISHER(PUBLISHER_ID INT,NAME VARCHAR(30),CITY VARCHAR(30),COUNTRY VARCHAR(30));
+CREATE TABLE AUTHOR(AUTHOR_ID INT,NAME VARCHAR(30),CITY VARCHAR(30),COUNTRY VARCHAR(30),PRIMARY KEY(AUTHOR_ID));
+CREATE TABLE PUBLISHER(PUBLISHER_ID INT,NAME VARCHAR(30),CITY VARCHAR(30),COUNTRY VARCHAR(30),PRIMARY KEY(PUBLISHER_ID));
+CREATE TABLE CATEGORY(CATEGORY_ID INT,DESCRIPTION VARCHAR(200), PRIMARY KEY(CATEGORY_ID));
+CREATE TABLE CATALOG(BOOK_ID INT,TITLE VARCHAR(50), AUTHOR_ID INT, PUBLISHER_ID INT,CATEGORY_ID INT,AYEAR INT,PRICE INT,FOREIGN KEY(AUTHOR_ID) REFERENCES AUTHOR(AUTHOR_ID) ON DELETE CASCADE, FOREIGN KEY(PUBLISHER_ID) REFERENCES PUBLISHER(PUBLISHER_ID) ON DELETE CASCADE, FOREIGN KEY(CATEGORY_ID) REFERENCES CATEGORY(CATEGORY_ID) ON DELETE CASCADE);
+CREATE TABLE CATALOG(BOOK_ID INT,TITLE VARCHAR(50), AUTHOR_ID INT, PUBLISHER_ID INT,CATEGORY_ID INT,AYEAR INT,PRICE INT,FOREIGN KEY(AUTHOR_ID) REFERENCES AUTHOR(AUTHOR_ID) ON DELETE CASCADE, FOREIGN KEY(PUBLISHER_ID) REFERENCES PUBLISHER(PUBLISHER_ID) ON DELETE CASCADE, FOREIGN KEY(CATEGORY_ID) REFERENCES CATEGORY(CATEGORY_ID) ON DELETE CASCADE, PRIMARY KEY(BOOK_ID));
+CREATE TABLE ORDERDETAILS(ORDER_NO INT,BOOK_ID INT, QUANTITY INT, FOREIGN KEY(BOOK_ID) REFERENCES CATALOG(BOOK_ID));
 
-CREATE TABLE Publisher(
-	publisher_id int,
-    P_name varchar(30),
-    city varchar(30),
-    country varchar(30),
-    PRIMARY KEY (publisher_id)
-);
-DESC Publisher;
+insert into author values(1001,"Teras Chan","CA","USA");
+insert into author values(1002,"Stevens","Zombi","Uganda");
+insert into author values(1003,"M Mano","Cair","Canada");
+insert into author values(1004,"Karthik B P","New York","USA");
+insert into author values(1005,"Willian Stalling","Las Vegas","USA");
 
-CREATE TABLE Catalog(
-	book_id int,
-    title varchar(30),
-    author_id varchar(30),
-    publisher_id int,
-    category_id int,
-    year int,
-    price int,
-    PRIMARY KEY (book_id),
-    FOREIGN KEY(publisher_id) references Publisher(publisher_id) ON DELETE CASCADE,
-    FOREIGN KEY(category_id) REFERENCES Category(category_id) ON DELETE CASCADE
-);
-DESC Catalog;
+insert into publisher values(1,"Pearson","New York","USA");
+insert into publisher values(2,"EEE","New South Vales","USA");
+insert into publisher values(3,"PHI","Delhi","India");
+insert into publisher values(4,"Willey","Berlin","Germany");
+insert into publisher values(5,"MGH","New York","USA");
 
-CREATE TABLE Category(
-	category_id int,
-    description varchar(30),
-    PRIMARY KEY(category_id)
-);
-DESC Category;
+insert into category values(1001,"Computer Science");
+insert into category values(1002,"Algorithm Design");
+insert into category values(1003,"Electronics");
+insert into category values(1004,"Programming");
+insert into category values(1005,"Operating Systems");
 
-CREATE TABLE Order_Details(
-	order_no int,
-    book_id int, 
-    quantity int,
-    PRIMARY KEY (Order_no),
-    FOREIGN KEY(book_id) REFERENCES Catalog(book_id) ON DELETE CASCADE
-);
-DESC Order_Details;
+insert into catalog values(11,"Unix System",1001,1,1001,2000,251);
+insert into catalog values(12,"Digital Signals",1002,2,1003,2001,425);
+insert into catalog values(13,"Logic Design",1003,3,1002,1999,225);
+insert into catalog values(14,"Server Prg",1004,4,1004,2001,333);
+insert into catalog values(15,"Linux OS",1005,5,1005,2003,326);
+insert into catalog values(16,"C++ Bible",1005,5,1001,2000,526);
+insert into catalog values(17,"Cobol Handbook",1005,4,1001,2000,658);
 
-INSERT INTO Author VALUES('11','Author1','A_city1','A_country1');
-commit;
-SELECT * FROM Author;
-INSERT INTO Author VALUES('12','Author2','A_city2','A_country2');
-INSERT INTO Author VALUES('13','Author3','A_city3','A_country3');
-INSERT INTO Author VALUES('14','Author4','A_city4','A_country4');
-INSERT INTO Author VALUES('15','Author5','A_city5','A_country5');
-commit;
+insert into orderdetails values(1,11,5);
+insert into orderdetails values(2,12,8);
+insert into orderdetails values(3,13,15);
+insert into orderdetails values(4,14,22);
+insert into orderdetails values(5,15,3);
+insert into orderdetails values(6,17,10);
 
-INSERT INTO Publisher VALUES('21','Publisher1','P_city1','P_country1');
-commit;
-INSERT INTO Publisher VALUES('22','Publisher2','P_city2','P_country2');
-INSERT INTO Publisher VALUES('23','Publisher3','P_city3','P_country3');
-INSERT INTO Publisher VALUES('24','Publisher4','P_city4','P_country4');
-INSERT INTO Publisher VALUES('25','Publisher5','P_city5','P_country5');
-commit;
+/*Give the details of the authors who have 2 or more books in the catalog and the price of the books in the
+catalog and the year of publication is after 2000.*/
 
-INSERT INTO Category VALUES('31','Category1');
-commit;
-INSERT INTO Category VALUES('32','Category2');
-INSERT INTO Category VALUES('33','Category3');
-INSERT INTO Category VALUES('34','Category4');
-INSERT INTO Category VALUES('35','Category5');
-commit;
+SELECT * FROM AUTHOR WHERE AUTHOR_ID IN (SELECT AUTHOR_ID FROM CATALOG WHERE AYEAR>2000 AND PRICE> (SELECT AVG(PRICE) FROM CATALOG) GROUP BY AUTHOR_ID HAVING COUNT(*)>1);
 
-INSERT INTO Catalog VALUES('41','Book1','12','24','31','2001','248');
-commit;
-INSERT INTO Catalog VALUES('42','Book2','15','21','33','2020','3480');
-INSERT INTO Catalog VALUES('43','Book3','11','23','31','1962','3569');
-INSERT INTO Catalog VALUES('44','Book4','13','25','33','2012','4720');
-INSERT INTO Catalog VALUES('45','Book5','14','24','35','2019','369');
-commit;
+/Find the author of the book which has maximum sales./
 
-INSERT INTO Order_Details VALUES('51','44','33');
-commit;
-INSERT INTO Order_Details VALUES('52','44','1');
-INSERT INTO Order_Details VALUES('53','43','38');
-INSERT INTO Order_Details VALUES('54','41','27');
-INSERT INTO Order_Details VALUES('55','43','68');
-commit;
+SELECT NAME FROM AUTHOR A,CATALOG C WHERE A.AUTHOR_ID=C.AUTHOR_ID AND BOOK_ID IN (SELECT BOOK_ID FROM ORDERDETAILS WHERE QUANTITY= (SELECT MAX(QUANTITY) FROM ORDERDETAILS));
 
--- **** Query 1: *****
-
-SELECT * FROM Author INNER JOIN Catalog ON Author.author_id = Catalog.author_id where (year >2000 AND price > 2000);
-
--- **** Query 2: *****
-
-SELECT * FROM Author INNER JOIN Catalog ON Author.author_id = Catalog.author_id AND book_id in(SELECT book_id FROM Order_Details WHERE quantity = (SELECT MAX(quantity) from Order_Details));
- 
--- **** Query 3: *****
-
-UPDATE Catalog SET price =(1+0.1)*price WHERE publisher_id in ( SELECT publisher_id FROM Publisher WHERE P_name ="Publisher3");
-SELECT * FROM Catalog;
+#Demonstrate how you increase the price of books published by a specific publisher by 10%.
+UPDATE CATALOG SET PRICE=1.1*PRICE WHERE PUBLISHER_ID IN (SELECT PUBLISHER_ID FROM PUBLISHER WHERE NAME='PEARSON');
